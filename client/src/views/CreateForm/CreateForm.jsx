0bbc1bd606502f037af968ee4df7b02c0eeba328
MBGonzalezMiramontes <begonzalezmiramontes@gmail.com>
2023-07-19 05:09:53 -0300

    algo de css

--- client/src/views/CreateForm/CreateForm.jsx
+++ client/src/views/CreateForm/CreateForm.jsx
@@ -13,6 +13,15 @@ const CreateForm = () => {
 
   const [errors, setErrors] = useState({});
 
+  const buttonDisabled = () => {
+    for (let error in errors) {
+      if (errors[error]) {
+        return true;
+      }
+    }
+    return false;
+  };
+
   const handleChange = (event) => {
     setErrors(
       Validations({
@@ -30,71 +39,85 @@ const CreateForm = () => {
     <div className={style.createFormMainContainer}>
       <form>
         <div className={style.inputContainer}>
-          <label>Nombre: </label>
+          <label className={style.label}>Nombre:</label>
           <input
+            className={style.input}
             name="name"
             value={activity.name}
             onChange={handleChange}
             type="text"
           />
-          {errors.noName ? (
-            <p>{errors.noName}</p>
-          ) : errors.nameLength ? (
-            <p>{errors.nameLength}</p>
-          ) : null}
         </div>
+        {errors.noName && <p className={style.error}>{errors.noName}</p>}
+        {errors.nameLength && (
+          <p className={style.error}>{errors.nameLength}</p>
+        )}
+
         <div className={style.inputContainer}>
-          <label>Dificultad: </label>
+          <label className={style.label}>Dificultad:</label>
           <input
+            className={style.input}
             name="difficulty"
             value={activity.difficulty}
             onChange={handleChange}
             type="text"
           />
-          {errors.nodifficulty ? (
-            <p>{errors.nodifficulty}</p>
-          ) : errors.difficultyLength ? (
-            <p>{errors.difficultyLength}</p>
-          ) : null}
         </div>
+        {errors.nodifficulty && (
+          <p className={style.error}>{errors.nodifficulty}</p>
+        )}
+        {errors.difficultyLength && (
+          <p className={style.error}>{errors.difficultyLength}</p>
+        )}
+
         <div className={style.inputContainer}>
-          <label>Duración: </label>
+          <label className={style.label}>Duración:</label>
           <input
+            className={style.input}
             name="duration"
             value={activity.duration}
             onChange={handleChange}
             type="text"
           />
-          {errors.invalidDuration ? (
-            <p>{errors.invalidDuration}</p>
-          ) : errors.invalidDurationRange ? (
-            <p>{errors.invalidDurationRange}</p>
-          ) : null}
         </div>
+        {errors.invalidDuration && (
+          <p className={style.error}>{errors.invalidDuration}</p>
+        )}
+        {errors.invalidDurationRange && (
+          <p className={style.error}>{errors.invalidDurationRange}</p>
+        )}
+
         <div className={style.inputContainer}>
-          <label>Temporada: </label>
+          <label className={style.label}>Temporada:</label>
           <input
+            className={style.input}
             name="season"
             value={activity.season}
             onChange={handleChange}
             type="text"
           />
-          {errors.noSeasonn ? (
-            <p>{errors.noSeason}</p>
-          ) : errors.invalidSeason ? (
-            <p>{errors.invalidSeason}</p>
-          ) : null}
         </div>
+        {errors.noSeason && <p className={style.error}>{errors.noSeason}</p>}
+        {errors.invalidSeason && (
+          <p className={style.error}>{errors.invalidSeason}</p>
+        )}
+
         <div className={style.inputContainer}>
-          <label>Descripción: </label>
-          <input
+          <label className={style.label}>Descripción:</label>
+          <textarea
+            className={`${style.input} ${style.descriptionInput}`}
             name="description"
             value={activity.description}
             onChange={handleChange}
-            type="text"
-          />
+          ></textarea>
         </div>
-        <input type="submit" />
+
+        <input
+          className={style.submitButton}
+          disabled={buttonDisabled()}
+          type="submit"
+          value="Enviar"
+        />
       </form>
     </div>
   );
